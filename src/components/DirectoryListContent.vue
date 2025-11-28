dc86e2257f06809cc111d990ce80b9a929e3f139
Meysam Azad <meysam@meysam.io>
2025-11-28 14:17:47 +0700

    fix: remove the idiotic magnifying glass that never works

--- src/components/DirectoryListContent.vue
+++ src/components/DirectoryListContent.vue
@@ -99,24 +99,6 @@
               >Search directories</label
             >
             <div class="relative w-full">
-              <div
-                class="absolute inset-y-0 left-0 w-10 flex items-center justify-center pointer-events-none"
-              >
-                <svg
-                  class="h-5 w-5 text-gray-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </div>
               <input
                 id="directory-search"
                 v-model="searchQuery"
